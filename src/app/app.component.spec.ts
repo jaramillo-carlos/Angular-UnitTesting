98e1fe6cf81298d522d4e284b3212d428a3f79b7
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-02 08:09:16 -0500

    test app component with dependencies

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,29 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import {MedicosComponent} from './components/medicos/medicos.component';
+import {CreacionComponent} from './components/medicos/creacion/creacion.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-      imports: [
-        FormsModule
-      ]
-    }).compileComponents();
+      declarations: [AppComponent, MedicosComponent, CreacionComponent],
+      imports: [FormsModule, HttpClientTestingModule]
+    })
+      .compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
