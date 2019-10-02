fd0dc78fc9746bbc6026e985f86e429f397d46e9
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-02 08:04:35 -0500

    test form creacion medicos component

--- src/app/components/medicos/creacion/creacion.component.spec.ts
+++ src/app/components/medicos/creacion/creacion.component.spec.ts
@@ -3,26 +3,53 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CreacionComponent} from './creacion.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MedicosService} from '../../../services/medicos.service';
 
 describe('CreacionComponent', () => {
-    let component: CreacionComponent;
-    let fixture: ComponentFixture<CreacionComponent>;
+  let component: CreacionComponent;
+  let fixture: ComponentFixture<CreacionComponent>;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            declarations: [CreacionComponent],
-            imports: [FormsModule, HttpClientTestingModule]
-        })
-            .compileComponents();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreacionComponent],
+      imports: [FormsModule, HttpClientTestingModule]
+    })
+      .compileComponents();
+  }));
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(CreacionComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('no debe guardar el medico cuando no tiene nombre o titulo', () => {
+    // Arrange
+    const medicosSVC = TestBed.get(MedicosService);
+    const agregarMedicoSpy = spyOn(medicosSVC, 'agregarMedico');
+    const alertSpy = spyOn(window, 'alert');
+    // Act
+    component.agregarMedico();
+    // Assert
+    expect(alertSpy).toHaveBeenCalledWith('Campos incompletos');
+    expect(agregarMedicoSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('debe agregar un medico', () => {
+    // Arrange
+    component.medico.nombre = 'Juan';
+    component.medico.titulo = 'Médico General';
+    const medicosSVC = TestBed.get(MedicosService);
+    const agregarMedicoSpy = spyOn(medicosSVC, 'agregarMedico').and.returnValue(Promise.resolve(medicosSVC));
+    const alertSpy = spyOn(window, 'alert');
+    // Act
+    component.agregarMedico();
+    // Assert
+    expect(agregarMedicoSpy).toHaveBeenCalledWith(component.medico);
+    expect(alertSpy).toHaveBeenCalledTimes(0);
+  });
 });
